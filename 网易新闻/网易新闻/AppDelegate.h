0a84e13900251fc628d736958b62f77280004754
liutingya <liutingya@liutingyademacbook-pro.local>
2016-03-13 23:33:45 +0800

    0 第一次

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by liutingya on 16/3/13.
+//  Copyright © 2016年 liutingya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
